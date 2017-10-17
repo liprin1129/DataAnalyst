b3ef4c5fe10c042cc40136d4f113c0f3addc0be7
SeongMuk Gang <pure@SeongMuks-MacBook-Pro.local>
2017-10-18 07:57:10 +0900

    Machine Learning project data and tools

---
+++ Lessons/5. Intro to Machine Learning/2. SVM/Q2. Different C parametre/C_10000_main.py
@@ -0,0 +1,45 @@
+import sys
+from class_vis import prettyPicture, output_image
+from prep_terrain_data import makeTerrainData
+
+import matplotlib.pyplot as plt
+import copy
+import numpy as np
+import pylab as pl
+
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+
+########################## SVM #################################
+### we handle the import statement and SVC creation for you here
+from sklearn.svm import SVC
+clf = SVC(kernel="rbf", C=10**6)
+
+
+#### now your job is to fit the classifier
+#### using the training features/labels, and to
+#### make a set of predictions on the test data
+clf.fit(features_train, labels_train)
+
+
+
+#### store your predictions in a list named pred
+pred = clf.predict(features_test)
+
+
+
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+
+if __name__ == "__main__":
+    print 'Accuracy: ', acc
+
+    ### draw the decision boundary with the text points overlaid
+    prettyPicture(clf, features_test, labels_test)
+    output_image("test.png", "png", open("test.png", "rb").read())
+'''
+def submitAccuracy():
+    return acc
+'''
