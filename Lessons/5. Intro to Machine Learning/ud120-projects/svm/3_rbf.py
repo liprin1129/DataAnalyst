e33c9604327279dde4238a0a215c2a00df46272f
SeongMuk Gang <pure@SeongMuks-MacBook-Pro.local>
2017-10-24 07:57:25 +0900

    Solve Decision Tree and SVM problem set.

--- Lessons/5. Intro to Machine Learning/ud120-projects/svm/3_rbf.py
+++ Lessons/5. Intro to Machine Learning/ud120-projects/svm/3_rbf.py
@@ -1,38 +1,47 @@
 #!/usr/bin/python
 
 """ 
-    This is the code to accompany the Lesson 3 (decision tree) mini-project.
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Use a SVM to identify emails from the Enron corpus by their authors:    
     Sara has label 0
     Chris has label 1
 """
     
 import sys
 from time import time
-#sys.path.append("../tools/")
+sys.path.append("../tools/")
 from email_preprocess import preprocess
 
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
+
 features_train, features_test, labels_train, labels_test = preprocess()
+
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
 
-from sklearn import tree
-clf = tree.DecisionTreeClassifier(min_samples_split=40)
-clf = clf.fit(features_train, labels_train)
+clf = SVC(kernel='rbf')
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
 pred = clf.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
 
 from sklearn.metrics import accuracy_score
-acc = accuracy_score(pred, labels_test)
 
-print "Accuracy: ", acc
+acc = accuracy_score(pred, labels_test)
 
 #########################################################
 
-
+if __name__ == "__main__":
+    print 'Accuracy: ', acc
