627a89eca6c27216eaf010329875bb7ed372c1e2
SeongMuk Gang <pure@SeongMuks-MacBook-Pro.local>
2017-12-05 22:43:36 +0900

    Project 3: csv to sql db finished

--- HomeWork/P3_Data_Wrangling/Project3_DataWrangling/csv2sql_db.py
+++ HomeWork/P3_Data_Wrangling/Project3_DataWrangling/csv2sql_db.py
@@ -1,14 +1,11 @@
 import sqlite3
 import csv
 import os
-os.chdir(r'/Users/pure/Private_Local_Data/Study/Udacity/DataAnalyst/HomeWork/P3_Data_Wrangling/Project3_DataWrangling/')
-
 from tqdm import tqdm
 
-"""
-    Create a connection of database to the SQLite database
-    """
+os.chdir(r'/Users/pure/Private_Local_Data/Study/Udacity/DataAnalyst/HomeWork/P3_Data_Wrangling/Project3_DataWrangling/')
 
+# Helper function
 def create_sql_connection(db_name):
     try:
         sql_connection = sqlite3.connect(db_name)
@@ -20,56 +17,133 @@ def create_sql_connection(db_name):
     return None
 
 def create_table(sql_connection, sql_command):
-    """
-        if conn:
-        command = '''
-        Create Table nodes(id INTEGER PRIMARY_KEY, lat REAL, lon REAL, user TEXT, uid INTEGER, version TEXT, changeset INTEGER, timestamp TEXT);
-        '''
-        """
     try:
         cursor = sql_connection.cursor()
         cursor.execute(sql_command)
     except ValueError:
         print "Error: ", ValueError
 
-# conn.commit
-
 def insert_data_to_table(sql_connection, sql_command, data):
-    #print list(node_dict)
-    #cursor.executemany("INSERT INTO nodes(id, lat, lon, user, uid, version, changeset, timestamp) VALUES (?,?,?,?,?,?,?,?);", node_dict.values())
     cursor = sql_connection.cursor()
-    #if len(data) != 1:
     cursor.execute(sql_command, data)
-#for i in node_dict:
-#print i['id']
 
+# Create connection
 sql_connection = create_sql_connection("data/sql_db/street_data.db")
-create_table(sql_connection, """
-    CREATE TABLE nodes(
+
+# nodes csv to sql db
+if False:
+    create_table(sql_connection, """
+        CREATE TABLE node(
+            id INTEGER PRIMARY_KEY,
+            lat REAL,
+            lon REAL,
+            user TEXT,
+            uid INTEGER PRIMARY_KEY,
+            version TEXT,
+            changeset INTEGER,
+            timestamp TEXT
+        );
+        """)
+
+    with sql_connection:
+        with open("data/csv/nodes.csv", "r") as node_csv:
+            node_dict = csv.DictReader(node_csv)
+
+            for row in tqdm(node_dict):
+                sql_command = "INSERT INTO node(id, lat, lon, user, uid, version, changeset, timestamp) VALUES (?,?,?,?,?,?,?,?);"
+                insert_data = (row["id"], row["lat"], row["lon"], row["user"].decode('utf8'), row["uid"], row["version"], row["changeset"], row["timestamp"])
+                
+                insert_data_to_table(sql_connection, sql_command, insert_data)
+
+# nodes tag csv to sql db
+if False:
+    create_table(sql_connection, """
+        CREATE TABLE node_tags(
         id INTEGER,
-        lat REAL,
-        lon REAL,
+        key TEXT,
+        value TEXT,
+        type TEXT,
+        FOREIGN KEY (id) REFERENCES nodes(id)
+        );
+        """)
+
+    with sql_connection:
+        with open("data/csv/nodes_tags.csv", "r") as node_csv:
+            node_dict = csv.DictReader(node_csv)
+            
+            for row in tqdm(node_dict):
+                sql_command = "INSERT INTO node_tags(id, key, value, type) VALUES (?,?,?,?);"
+                insert_data = (row["id"], row["key"].decode('utf8'), row["value"].decode('utf8'), row["type"].decode('utf8'))
+                
+                insert_data_to_table(sql_connection, sql_command, insert_data)
+
+# way csv to sql db
+if False:
+    create_table(sql_connection, """
+        CREATE TABLE way(
+        id INTEGER,
         user TEXT,
-        uid INTEGER PRIMARY_KEY,
+        uid INTEGER,
         version TEXT,
+        timestamp TEXT,
         changeset INTEGER,
-        timestamp TEXT
-    );
-    """)
+        PRIMARY KEY(id, uid)
+        );
+        """)
 
-with sql_connection:
-    '''
-    with open("data/csv/nodes.csv", "r") as node_csv:
-        #node_dict = csv.DictReader(node_csv)
-        node_dict = csv.reader(node_csv)
-    '''
-    import pandas
-    df = pandas.read_csv("data/csv/nodes.csv")
-    df.to_sql("nodes", sql_connection, if_exists='append', index=False)
-    '''
-        sql_command = "INSERT INTO nodes(id, lat, lon, user, uid, version, changeset, timestamp) VALUES (?,?,?,?,?,?,?,?);"
-        insert_data = (row["id"], row["lat"], row["lon"], row["user"], row["uid"], row["version"], row["changeset"], row["timestamp"])
-        
-        insert_data_to_table(sql_connection, sql_command, insert_data)
+    with sql_connection:
+        with open("data/csv/ways.csv", "r") as node_csv:
+            node_dict = csv.DictReader(node_csv)
+            
+            for row in tqdm(node_dict):
+                #print row
 
-    '''
+                sql_command = "INSERT INTO way(id, user, uid, version, timestamp, changeset) VALUES (?,?,?,?,?,?);"
+                insert_data = (row["id"], row["user"].decode('utf8'), row["uid"], row["version"].decode('utf8'), row["timestamp"].decode('utf8'), row["changeset"])
+                
+                insert_data_to_table(sql_connection, sql_command, insert_data)
+
+# way nodes csv to sql db
+if False:
+    create_table(sql_connection, """
+        CREATE TABLE way_nodes(
+        id INTEGER,
+        node_id INTEGER,
+        position INTEGER,
+        FOREIGN KEY (id) REFERENCES ways(id)
+        );
+        """)
+    
+    with sql_connection:
+        with open("data/csv/ways_nodes.csv", "r") as node_csv:
+            node_dict = csv.DictReader(node_csv)
+            
+            for row in tqdm(node_dict):
+                
+                sql_command = "INSERT INTO way_nodes(id, node_id, position) VALUES (?,?,?);"
+                insert_data = (row["id"], row["node_id"], row["position"])
+                
+                insert_data_to_table(sql_connection, sql_command, insert_data)
+
+# way tags csv to sql db
+if False:
+    create_table(sql_connection, """
+        CREATE TABLE way_tags(
+        id INTEGER,
+        key TEXT,
+        type TEXT,
+        value TEXT,
+        FOREIGN KEY (id) REFERENCES ways(id)
+        );
+        """)
+    
+    with sql_connection:
+        with open("data/csv/ways_tags.csv", "r") as node_csv:
+            node_dict = csv.DictReader(node_csv)
+            
+            for row in tqdm(node_dict):
+                
+                sql_command = "INSERT INTO way_tags(id, key, type, value) VALUES (?,?,?,?);"
+                insert_data = (row["id"], row["key"].decode('utf8'), row["type"].decode('utf8'), row["value"].decode('utf8'))
+                
+                insert_data_to_table(sql_connection, sql_command, insert_data)
