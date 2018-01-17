fd605e267d2129ed0b217ce8e6da46b187acfa4e
SeongMuk Gang <pure@SeongMuks-MacBook-Pro.local>
2018-01-18 08:07:35 +0900

    ML project

--- HomeWork/P5_ML/final_project/feature_exploration.py
+++ HomeWork/P5_ML/final_project/feature_exploration.py
@@ -8,11 +8,16 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 import pandas as pd
 
-words_file = "final_project_dataset_modified.pkl" 
-authors_file = "final_project_dataset.pkl"
-word_data = pickle.load( open(words_file, "r"))
-authors = pickle.load( open(authors_file, "r") )
+fpm = "final_project_dataset_modified.pkl" 
+fp = "final_project_dataset.pkl"
+
+fpm_df = pickle.load( open(fpm, "r"))
+fp_df = pickle.load( open(fp, "r") )
 
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
+fpm_df = pd.DataFrame(fpm_df)
+fp_df = pd.DataFrame(fp_df)
+#print type(fpm_df), type(fp_df)
+print fpm_df.head()
 features_list = ['poi','salary'] # You will need to use more features
